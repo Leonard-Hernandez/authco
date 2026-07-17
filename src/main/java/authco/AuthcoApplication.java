6b5397a9bf6d2e8466b7745fcf6556093cae1de7
leonardh <leonardhernandez02@gmail.com>
2026-07-16 23:35:49 -0500

    follow the learning path

---
+++ src/main/java/authco/AuthcoApplication.java
@@ -0,0 +1,13 @@
+package authco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/authco/AuthcoApplicationTests.java
@@ -0,0 +1,13 @@
+package authco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
